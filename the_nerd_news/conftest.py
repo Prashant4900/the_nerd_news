73af605c0890892807ba978d673b12d2e86b9dad
Prashant Nigam <62440911+Prashant4900@users.noreply.github.com>
2022-11-16 16:15:36 +0530

    Initial commit

---
+++ the_nerd_news/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from the_nerd_news.users.models import User
+from the_nerd_news.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
